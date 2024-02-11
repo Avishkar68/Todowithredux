e9a365d690f809cb779882ce881858cbe9f174d8
Avishkar-Kakade <kakadeavishkar84@gmail.com>
2024-02-12 02:13:12 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h2>Todo with React Redux Toolkit</h2>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
